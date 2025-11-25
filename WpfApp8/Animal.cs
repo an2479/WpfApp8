8e37e7378a40d9a6f97b7eeffc9b929bec892170
User <User@WXI1146440>
2025-11-25 15:35:39 +0200

    Add project files.

---
+++ WpfApp8/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp8
+{
+    public class Animal
+    {
+        private string name;
+        private DateTime birth;
+        private string type;
+        private string gender;
+        private double weight;
+        public Animal(string name, DateTime birth, string type, string gender, double weight)
+        {
+            this.name = name;
+            this.birth = birth;
+            this.type = type;
+            this.gender = gender;
+            this.weight = weight;
+        }
+        public string Name { get => name; set => name = value; }
+        public DateTime Birth { get => birth; set => birth = value; }
+        public string Type { get => type; set => type = value; }
+        public string Gender { get => gender; set => gender = value; }
+        public double Weight { get => weight; set => weight = value; }
+        public override string ToString()
+        {
+            return $"name: {name},birth: {birth},type: {type},gender: {gender},weight: {weight}";
+        }
+    }
+}
---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submit(object sender, RoutedEventArgs e)
+        {
+            TextBlock show = new TextBlock();
+            show.Text = "Show";
+            show.FontSize = 40;
+            sp.Children.Add(show);
+            List<Animal> list = new List<Animal>();
+            try
+            {
+                list.Add(new Animal(name.Text, DateTime.Parse(birth.Text), type.Text, gender.Text, double.Parse(weight.Text)));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            //sp.ItemsSource = list;
+            //name.Text = (name.SelectedItem as Animal).Name;
+        }
+    }
+}
